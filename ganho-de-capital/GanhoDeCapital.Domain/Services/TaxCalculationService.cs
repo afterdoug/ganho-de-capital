2763fdf55dafcbae5c58b9967c72d7622bf3a842
Douglas Godoi Ribeiro <douglasgr.ciandt@fdc.org.br>
2025-10-05 22:30:07 -0300

    Simplificando o Service.

--- ganho-de-capital/GanhoDeCapital.Domain/Services/TaxCalculationService.cs
+++ ganho-de-capital/GanhoDeCapital.Domain/Services/TaxCalculationService.cs
@@ -11,41 +11,25 @@ public class TaxCalculationService : ITaxCalculationService
 
     public TaxCalculationService(IEnumerable<KeyValuePair<OperationType, IOperationStrategy>> strategies = null)
     {
-        if (strategies != null)
-        {
-            _strategies = new Dictionary<OperationType, IOperationStrategy>(strategies);
-        }
-        else
-        {
-            _strategies = new Dictionary<OperationType, IOperationStrategy>
+        _strategies = new Dictionary<OperationType, IOperationStrategy>
             {
                 { OperationType.Buy, new BuyOperationStrategy() },
                 { OperationType.Sell, new SellOperationStrategy() }
             };
-        }
     }
 
     public IEnumerable<TaxCalculationResult> CalculateTaxes(IEnumerable<StockOperation> operations)
     {
-        if (operations == null)
-            throw new ArgumentNullException(nameof(operations));
-
         var results = new List<TaxCalculationResult>();
         var position = new Position();
 
         foreach (var operation in operations)
         {
-            if (_strategies.TryGetValue(operation.Operation, out var strategy))
-            {
-                var result = strategy.CalculateTax(
-                    operation, ref position);
-                
-                results.Add(result);
-            }
-            else
-            {
-                throw new InvalidOperationException($"Unsupported operation type: {operation.Operation}");
-            }
+            var strategy = _strategies.GetValueOrDefault(operation.Operation);
+
+            var result = strategy!.CalculateTax(operation, ref position);
+
+            results.Add(result);
         }
         
         return results;
